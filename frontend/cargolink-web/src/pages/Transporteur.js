814f3dde7bdb8b1c0b5e275805a45bd04f2b0524
EXDIVIA SARL <atanganaoteleg@yahoo.fr>
2026-07-18 15:42:53 +0100

    Import VILLES Transporteur

--- frontend/cargolink-web/src/pages/Transporteur.js
+++ frontend/cargolink-web/src/pages/Transporteur.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import VILLES from '../data/villes';
 import { getDemandesDisponibles, accepterDemande } from '../services/api';
 
 function Transporteur({ user }) {

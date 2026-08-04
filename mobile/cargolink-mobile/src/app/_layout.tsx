86ecfcdc342fbb82234919c8fbe0ea571e3acd43
EXDIVIA SARL <atanganaoteleg@yahoo.fr>
2026-08-04 14:38:28 +0100

    Fix layout tsx routes explicites

--- mobile/cargolink-mobile/src/app/_layout.tsx
+++ mobile/cargolink-mobile/src/app/_layout.tsx
@@ -1,5 +1,16 @@
 import { Stack } from 'expo-router';
+import { useEffect } from 'react';
 
 export default function Layout() {
-  return <Stack screenOptions={{ headerShown: false }}/>
+  return (
+    <Stack>
+      <Stack.Screen name="index" options={{ headerShown: false }} />
+      <Stack.Screen name="inscription" options={{ headerShown: false }} />
+      <Stack.Screen name="connexion" options={{ headerShown: false }} />
+      <Stack.Screen name="dashboard" options={{ headerShown: false }} />
+      <Stack.Screen name="transporteur" options={{ headerShown: false }} />
+      <Stack.Screen name="mes-demandes" options={{ headerShown: false }} />
+      <Stack.Screen name="mes-demandes-transporteur" options={{ headerShown: false }} />
+    </Stack>
+  );
 }

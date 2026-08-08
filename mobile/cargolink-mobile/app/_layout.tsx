20878e9459ea2ef5de4832f35c82f5532f04a67d
EXDIVIA SARL <atanganaoteleg@yahoo.fr>
2026-08-08 18:12:45 +0100

    Fix correction layout

--- mobile/cargolink-mobile/app/_layout.tsx
+++ mobile/cargolink-mobile/app/_layout.tsx
@@ -1,5 +1,18 @@
 import { Stack } from 'expo-router';
 
+// @ts-ignore
+const defaultHandler = global.ErrorUtils?.getGlobalHandler?.();
+// @ts-ignore
+if (global.ErrorUtils) {
+  // @ts-ignore
+  global.ErrorUtils.setGlobalHandler((error, isFatal) => {
+    const msg = 'ERREUR: ' + (error?.message || 'inconnue') + '\n\nSTACK:\n' + (error?.stack || '');
+    console.log(msg);
+    alert(msg);
+    if (defaultHandler) defaultHandler(error, isFatal);
+  });
+}
+
 export default function Layout() {
   return (
     <Stack initialRouteName="index">
@@ -7,7 +20,7 @@ export default function Layout() {
       <Stack.Screen name="inscription" options={{ headerShown: false }} />
       <Stack.Screen name="connexion" options={{ headerShown: false }} />
       <Stack.Screen name="dashboard" options={{ headerShown: false }} />
-      <Stack.Screen name="transporteur" options={{ headerShown: false }} />
+      <Stack.Screen name="transporteurs" options={{ headerShown: false }} />
       <Stack.Screen name="mes-demandes" options={{ headerShown: false }} />
       <Stack.Screen name="mes-demandes-transporteur" options={{ headerShown: false }} />
     </Stack>
